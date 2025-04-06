de4c1c375bf8e4a81dfe7bf24e3e13d9757969e7
odilboooy10 <odilkhon318@gmail.com>
2025-04-06 18:09:19 +0900

    feat: develop GoCar monorepo project

--- apps/gocar-batch/src/gocar-batch.service.ts
+++ apps/gocar-batch/src/gocar-batch.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class GocarBatchService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to GoCar Batch Server';
   }
 }
---
+++ apps/gocar-batch/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { GocarBatchModule } from './gocar-batch.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(GocarBatchModule);
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
